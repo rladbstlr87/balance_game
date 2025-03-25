777c35ffb0e44d8755293205a8edc057242e0b58
rladbstlr87 <manylong@naver.com>
2025-03-25 10:36:27 +0900

    migrate

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    A = models.CharField(max_length=100)
+    B = models.CharField(max_length=100)
+
+    
